e3ef62d55dd52d9de39a25f038b65923cbea448e
Adrian Canals <a.canals14@gmail.com>
2024-05-17 21:34:37 +0200

    estuctura del paquete en laravel

---
+++ database/migrations/0000_00_00_000004_create_states_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('finalized');
+            $table->string('class')->unique();
+            $table->string('type');
+            $table->string('icon')->nullable();
+            $table->timestamps();
+            $table->unique(["class", "type"], 'class_type_unique');
+            $table->unique(["name", "type"], 'name_type_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
