f753f4e11ffbcbe77702f0087ad1b289b98d727d
Adrian Canals <a.canals14@gmail.com>
2024-06-18 12:46:15 +0200

    [7.x] Quitar prefijo "hexagonal." del nombre de las rutas porque ya se añade al registrarlas

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxQueuesController;
 use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxWebsocketsController;
 
 
-Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('hexagonal.ajax.queues.checkService');
-Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('hexagonal.ajax.websockets.checkService');
+Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
+Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
 
-Route::get('/ajax/queues/jobs',                 [AjaxJobsController::class, 'getJobs'])->name('hexagonal.ajax.queues.getJobs');
-Route::get('/ajax/queues/failed-jobs',          [AjaxJobsController::class, 'getFailedJobs'])->name('hexagonal.ajax.queues.getFailedJobs');
+Route::get('/ajax/queues/jobs',                 [AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
+Route::get('/ajax/queues/failed-jobs',          [AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
