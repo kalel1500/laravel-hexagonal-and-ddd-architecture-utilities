a975457ab43a14cba88c8eea805f386ae69170f7
Adrian Canals <a.canals14@gmail.com>
2024-06-18 14:04:30 +0200

    [7.x] Nuevas rutas "queues.queuedJobs" y "queues.failedJobs" que solo devuelven una vista html con un id para tabulator

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ use Illuminate\Support\Facades\Route;
 use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxJobsController;
 use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxQueuesController;
 use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxWebsocketsController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\JobsController;
 
 
 Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
 Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
 
+
+Route::get('/queues/jobs',                      [JobsController::class, 'queuedJobs'])->name('queues.queuedJobs');
+Route::get('/queues/failed-jobs',               [JobsController::class, 'failedJobs'])->name('queues.failedJobs');
 Route::get('/ajax/queues/jobs',                 [AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
 Route::get('/ajax/queues/failed-jobs',          [AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
