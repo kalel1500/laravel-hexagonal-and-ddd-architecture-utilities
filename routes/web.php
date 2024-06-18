53e67cc5d8dfefcc35c8714fff01303b9dc3f3e5
Adrián <acanals@externos.itnow.es>
2024-06-18 12:31:25 +0200

    [7.x] añadir las rutas "queues.checkService" y "websockets.checkService" al paquete

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxQueuesController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\AjaxWebsocketsController;
 
+
+Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('hexagonal.ajax.queues.checkService');
+Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('hexagonal.ajax.websockets.checkService');
