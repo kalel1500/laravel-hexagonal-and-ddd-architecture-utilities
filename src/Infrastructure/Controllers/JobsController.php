2f07375ded4c5274c625a1a284c3101ae6ade82e
Adrián <acanals@externos.itnow.es>
2024-07-16 15:22:36 +0200

    ServiceProvider: registrar vistas para poder llamarlas con el método "view()" desde el controller (antes View::file(__DIR__.'/...))

--- src/Infrastructure/Controllers/JobsController.php
+++ src/Infrastructure/Controllers/JobsController.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Hexagonal\Infrastructure\Controllers;
 
-
-use Illuminate\Support\Facades\View;
-
 final class JobsController extends Controller
 {
     public function queuedJobs(): \Illuminate\Contracts\View\View
     {
-        return View::file(__DIR__.'/../../../resources/views/queues/jobs.blade.php');
+        return view('hexagonal::queues.jobs');
     }
 
     public function failedJobs(): \Illuminate\Contracts\View\View
     {
-        return View::file(__DIR__.'/../../../resources/views/queues/jobs.blade.php');
+        return view('hexagonal::queues.jobs');
     }
 }
