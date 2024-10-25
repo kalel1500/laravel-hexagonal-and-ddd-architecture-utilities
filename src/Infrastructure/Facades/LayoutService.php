855267431524581964294fcc70e3a47b14ed76b8
Adrián <acanals@externos.itnow.es>
2024-10-25 19:12:25 +0200

    componentesLayout05: crear clase LayoutService y su Facade

---
+++ src/Infrastructure/Facades/LayoutService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thehouseofel\Hexagonal\Infrastructure\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Thehouseofel\Hexagonal\Domain\Objects\DataObjects\Layout\UserInfoDo;
+
+/**
+ * @method static int getMessageCounter()
+ * @method static mixed getNavbarNotifications()
+ * @method static UserInfoDo getUserInfo()
+ */
+final class LayoutService extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'layoutService';
+    }
+}
