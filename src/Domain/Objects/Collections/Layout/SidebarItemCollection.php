80356b6e5a20dcca38bf7fa16098a7e31b62608e
Adrian Canals <a.canals14@gmail.com>
2024-10-25 19:10:57 +0200

    componentesLayout04: crear clases DTOs

---
+++ src/Domain/Objects/Collections/Layout/SidebarItemCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thehouseofel\Hexagonal\Domain\Objects\Collections\Layout;
+
+use Thehouseofel\Hexagonal\Domain\Objects\Collections\Contracts\ContractCollectionDo;
+use Thehouseofel\Hexagonal\Domain\Objects\DataObjects\Layout\SidebarItemDo;
+
+final class SidebarItemCollection extends ContractCollectionDo
+{
+    protected const VALUE_CLASS = SidebarItemDo::class;
+
+    public function __construct(SidebarItemDo ...$items)
+    {
+        $this->items = $items;
+    }
+}
