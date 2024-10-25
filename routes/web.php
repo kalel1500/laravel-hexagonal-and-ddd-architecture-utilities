dbb34fa203176f2d4268073b7309a733b62426f3
Adrian Canals <a.canals14@gmail.com>
2024-10-25 17:50:46 +0200

    Cambiar los imports por rutas absolutas en el "web.php"

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,20 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController;
-use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxQueuesController;
-use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxWebsocketsController;
-use Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController;
 
 /**
  * Service routes
  */
-Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
-Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
+Route::get('/ajax/check-service-queues',        [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
+Route::get('/ajax/check-service-websockets',    [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
 
 /**
  * Queues routes
  */
-Route::get('/queues/jobs',                      [JobsController::class, 'queuedJobs'])->name('queues.queuedJobs');
-Route::get('/queues/failed-jobs',               [JobsController::class, 'failedJobs'])->name('queues.failedJobs');
-Route::get('/ajax/queues/jobs',                 [AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
-Route::get('/ajax/queues/failed-jobs',          [AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
+Route::get('/queues/jobs',                      [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController::class, 'queuedJobs'])->name('queues.queuedJobs');
+Route::get('/queues/failed-jobs',               [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController::class, 'failedJobs'])->name('queues.failedJobs');
+Route::get('/ajax/queues/jobs',                 [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
+Route::get('/ajax/queues/failed-jobs',          [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
 
 /**
  * Test routes
