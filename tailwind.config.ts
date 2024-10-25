7b09e129010bce54e02993f9f77b08635c61c7f3
Adrián <acanals@externos.itnow.es>
2024-10-25 23:27:36 +0200

    compilarResources03: archivos de configuración de tailwind, vite, y typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin';
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.ts",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'vsm': '440px'
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    plugin(function ({ addVariant }) {
+      addVariant('sc', '&:is(.sc *)');
+    })
+  ],
+} satisfies Config
+
