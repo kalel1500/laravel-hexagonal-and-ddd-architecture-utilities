a12035ee581d6db0c43043962c249c9e7759820f
Adrian Canals <a.canals14@gmail.com>
2024-10-25 23:26:14 +0200

    compilarResources02: mejorar método "LayoutController@public" para obtener los assets con hash

--- src/Infrastructure/Controllers/Web/LayoutController.php
+++ src/Infrastructure/Controllers/Web/LayoutController.php
@@ -4,17 +4,35 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Hexagonal\Infrastructure\Controllers\Web;
 
+use Illuminate\Support\Facades\File;
 use Thehouseofel\Hexagonal\Infrastructure\Controllers\Controller;
 
 final class LayoutController extends Controller
 {
-    public function public($type, $file)
+    public function public($file)
     {
-        $path = HEXAGONAL_PATH . "/public/{$type}/{$file}";
+        // Ruta al archivo manifest.json del paquete
+        $manifestPath = HEXAGONAL_PATH . '/public/build/manifest.json';
 
-        if (file_exists($path)) {
-            return response()->file($path);
+        // Comprobar que el archivo manifest existe
+        if (!File::exists($manifestPath)) {
+            abort(404, 'Manifest file not found.');
         }
+
+        // Leer y decodificar el manifest.json
+        $manifest = json_decode(File::get($manifestPath), true);
+
+        // Verificar si el archivo solicitado está en el manifest
+        if (isset($manifest[$file])) {
+            // Obtener el path generado del archivo con hash
+            $hashedFilePath = HEXAGONAL_PATH . '/public/build/' . $manifest[$file]['file'];
+
+            // Comprobar que el archivo con hash existe
+            if (File::exists($hashedFilePath)) {
+                return response()->file($hashedFilePath);
+            }
+        }
+
         abort(404);
     }
 }
