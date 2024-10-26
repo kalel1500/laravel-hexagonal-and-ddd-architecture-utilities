b5c79c3489f0347c1cfcc3cad3840e1d0e351458
Adrian Canals <a.canals14@gmail.com>
2024-10-26 02:26:43 +0200

    compilarResources05: archivos iniciales TS y CSS

---
+++ resources/js/app/constants.ts
@@ -0,0 +1,15 @@
+import { Constants, DefaultConstants } from 'laravel-ts-utilities';
+
+interface AppConstants extends DefaultConstants {
+    anotherSetting: string;
+}
+
+const constants = Constants.getInstance<AppConstants>();
+constants.extend({
+    VITE_APP_ENV:               import.meta.env.VITE_APP_ENV,
+    VITE_APP_NAME:              import.meta.env.VITE_APP_NAME,
+    VITE_APP_STORAGE_VERSION:   import.meta.env.VITE_APP_STORAGE_VERSION,
+    anotherSetting:             'newCustomValue',
+});
+
+export const _const = <T extends keyof AppConstants>(key: T): AppConstants[T] => constants.get(key);
