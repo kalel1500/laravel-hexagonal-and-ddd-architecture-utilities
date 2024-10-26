8ed22a78ca4429dee565b6e404244510fb395423
Adrian Canals <a.canals14@gmail.com>
2024-10-26 05:05:04 +0200

    compilarResources06: (refactor) actualizar "tailwind.config.ts"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,16 @@
 import type { Config } from 'tailwindcss'
-import plugin from 'tailwindcss/plugin';
+import { laravelContent, laravelDefaultPlugins } from 'laravel-ts-utilities/dist/plugins';
 
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.ts",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js",
+    ...laravelContent
   ],
   theme: {
     extend: {
-      screens: {
-        'vsm': '440px'
-      }
+      screens: {}
     },
   },
   plugins: [
-    require('flowbite/plugin'),
-    plugin(function ({ addVariant }) {
-      addVariant('sc', '&:is(.sc *)');
-    })
+    ...laravelDefaultPlugins
   ],
 } satisfies Config
-
