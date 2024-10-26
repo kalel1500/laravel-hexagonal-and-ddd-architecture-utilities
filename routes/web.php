6cf03ba7041dbd9953261eb57ab5d942d13462b5
Adrián <acanals@externos.itnow.es>
2024-10-26 02:13:36 +0200

    (refactor) importar los controllers en las Rutas

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,42 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxQueuesController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxWebsocketsController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\LayoutController;
+use Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\TestController;
+
 
 /**
  * Service routes
  */
-Route::get('/ajax/check-service-queues',        [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
-Route::get('/ajax/check-service-websockets',    [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
+
+Route::get('/ajax/check-service-queues',        [AjaxQueuesController::class, 'checkService'])->name('ajax.queues.checkService');
+Route::get('/ajax/check-service-websockets',    [AjaxWebsocketsController::class, 'checkService'])->name('ajax.websockets.checkService');
+
 
 /**
  * Queues routes
  */
-Route::get('/queues/jobs',                      [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController::class, 'queuedJobs'])->name('queues.queuedJobs');
-Route::get('/queues/failed-jobs',               [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\JobsController::class, 'failedJobs'])->name('queues.failedJobs');
-Route::get('/ajax/queues/jobs',                 [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
-Route::get('/ajax/queues/failed-jobs',          [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Ajax\AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
+
+Route::get('/queues/jobs',                      [JobsController::class, 'queuedJobs'])->name('queues.queuedJobs');
+Route::get('/queues/failed-jobs',               [JobsController::class, 'failedJobs'])->name('queues.failedJobs');
+Route::get('/ajax/queues/jobs',                 [AjaxJobsController::class, 'getJobs'])->name('ajax.queues.getJobs');
+Route::get('/ajax/queues/failed-jobs',          [AjaxJobsController::class, 'getFailedJobs'])->name('ajax.queues.getFailedJobs');
+
 
 /**
  * Test routes
  */
-Route::get('/test',                             [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\TestController::class, 'test'])->name('test');
+
+Route::get('/test',                             [TestController::class, 'test'])->name('test');
+
 
 /**
  * Layout routes
  */
+
 // Definir una ruta para servir los assets del paquete
-Route::get('/public/assets/{file}',             [\Thehouseofel\Hexagonal\Infrastructure\Controllers\Web\LayoutController::class, 'public'])
-    ->where('file', '.*')
-    ->name('public');
+Route::get('/public/assets/{file}',             [LayoutController::class, 'public'])->where('file', '.*')->name('public');
