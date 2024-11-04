ff4e51fee12a762abcb122d252d0cccf6581f74a
Adrián <acanals@externos.itnow.es>
2024-11-04 15:48:00 +0100

    (fix) corregir error al pasar el antiguo parámetro HTTP_CODE en el constructor de la clase "UnsetRelationException"

--- src/Domain/Exceptions/Database/UnsetRelationException.php
+++ src/Domain/Exceptions/Database/UnsetRelationException.php
@@ -12,6 +12,6 @@ final class UnsetRelationException extends BasicException
 
     public function __construct(string $relation = '', string $entity = '')
     {
-        parent::__construct(sprintf('Call to relation [%s] that was not set when creating Entity [%s]', $relation, $entity), HTTP_INTERNAL_SERVER_ERROR());
+        parent::__construct(sprintf('Call to relation [%s] that was not set when creating Entity [%s]', $relation, $entity));
     }
 }
