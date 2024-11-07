c48006ba9131e3dd05586f9775be4f2f48bf6aa8
Adrián <acanals@externos.itnow.es>
2024-11-07 18:16:26 +0100

    Nuevo comando ("HexagonalStart") creado para crear los archivos iniciales en la aplicación

---
+++ src/Infrastructure/Console/Commands/HexagonalStart.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Thehouseofel\Hexagonal\Infrastructure\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class HexagonalStart extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hexagonal:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create starter files for hexagonal architecture';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+    }
+}
