120a963f82c944a2104f3f179118311b689f1be7
Adrián <acanals@externos.itnow.es>
2024-11-07 18:16:49 +0100

    HexagonalStart: añadir la creación de los archivos iniciales -> + app/Providers/DependencyServiceProvider.php + resources/views/pages/home/index.blade.php + src/Home/Infrastructure/HomeController.php + src/Shared/Domain/Services/RepositoryServices/LayoutService.php + .env.local

--- src/Infrastructure/Console/Commands/HexagonalStart.php
+++ src/Infrastructure/Console/Commands/HexagonalStart.php
@@ -3,6 +3,7 @@
 namespace Thehouseofel\Hexagonal\Infrastructure\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class HexagonalStart extends Command
 {
@@ -20,11 +21,39 @@ class HexagonalStart extends Command
      */
     protected $description = 'Create starter files for hexagonal architecture';
 
+    protected $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        parent::__construct();
+        $this->filesystem = $filesystem;
+    }
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        /**
+         * --- Creación de archivos ---
+         */
 
+        // DependencyServiceProvider
+        copy(HEXAGONAL_PATH.'/stubs/app/Providers/DependencyServiceProvider.php', app_path('Providers/DependencyServiceProvider.php'));
+        $this->info('Archivo "app/Providers/DependencyServiceProvider.php" creado');
+
+        // resources
+        $this->filesystem->ensureDirectoryExists(resource_path('views/pages'));
+        $this->filesystem->copyDirectory(HEXAGONAL_PATH.'/stubs/resources/views/pages', resource_path('views/pages'));
+        $this->info('Carpeta "resources/views/pages" creada');
+
+        // Src
+        $this->filesystem->ensureDirectoryExists(base_path('src'));
+        $this->filesystem->copyDirectory(HEXAGONAL_PATH.'/stubs/src', base_path('src'));
+        $this->info('Carpeta "src" creada');
+
+        // .env.local
+        copy(HEXAGONAL_PATH.'/stubs/.env.local', base_path('.env.local'));
+        $this->info('Archivo ".env.local" creado');
     }
 }
