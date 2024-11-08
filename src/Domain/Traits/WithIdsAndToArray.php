0a2ef4e824fb168ed65e2315bb615aa7b0031ac5
Adrian Canals <a.canals14@gmail.com>
2024-11-08 23:03:39 +0100

    Nuevo método estático "fromId()" en el trait "WithIdsAndToArray" para poder instanciar un "BackedEnum" a partir del "Id"

--- src/Domain/Traits/WithIdsAndToArray.php
+++ src/Domain/Traits/WithIdsAndToArray.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Hexagonal\Domain\Traits;
 
+/**
+ * @template TEnum of \BackedEnum
+ */
 trait WithIdsAndToArray
 {
     // Método para obtener el ID asociado a un valor del enum
     public function getId(): int {
         return static::values()[$this->value];
+    }
+
+    /**
+     * @param int $id
+     * @return TEnum
+     */
+    public static function fromId(int $id) {
+        $value = array_search($id, static::values(), true);
+        if ($value === false) {
+            $message = sprintf('"%s" is not a valid backing value for enum %s', $id, static::class);
+            throw new \ValueError($message);
+        }
+        return static::from($value);
     }
 
     // Método para convertir todos los valores del enum en un array de strings
