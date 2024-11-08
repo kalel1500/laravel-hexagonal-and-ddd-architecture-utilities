665f3f5695b4f86a788c4ca224ae2ac1d4fcb2b5
Adrian Canals <a.canals14@gmail.com>
2024-11-08 10:40:55 +0100

    HexagonalStart: modificación de archivos -> + añadir el "DependencyServiceProvider" en "/bootstrap/providers.php" + añadir el "ExceptionHandler" en "/bootstrap/app.php"

--- src/Infrastructure/Console/Commands/HexagonalStart.php
+++ src/Infrastructure/Console/Commands/HexagonalStart.php
@@ -4,6 +4,9 @@ namespace Thehouseofel\Hexagonal\Infrastructure\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class HexagonalStart extends Command
 {
@@ -55,5 +58,49 @@ class HexagonalStart extends Command
         // .env.local
         copy(HEXAGONAL_PATH.'/stubs/.env.local', base_path('.env.local'));
         $this->info('Archivo ".env.local" creado');
+
+
+        /**
+         * --- Modificación de archivos ---
+         */
+
+        // /bootstrap/providers.php
+        ServiceProvider::addProviderToBootstrapFile('App\Providers\DependencyServiceProvider');
+        $this->info('Archivo "/bootstrap/providers.php" modificado');
+
+        // /bootstrap/app.php
+        $this->installException([
+            '$callback = \Thehouseofel\Hexagonal\Infrastructure\Exceptions\ExceptionHandler::getUsingCallback();',
+            '$callback($exceptions);',
+        ]);
+        $this->info('Archivo "/bootstrap/app.php" modificado');
+    }
+
+    /**
+     * Install the given exception names into the application.
+     *
+     * @param  array|string  $names
+     * @return void
+     */
+    protected function installException($names)
+    {
+        $bootstrapApp = file_get_contents(base_path('bootstrap/app.php'));
+
+        $names = collect(Arr::wrap($names))
+            ->filter(function ($name) use ($bootstrapApp) { return ! Str::contains($bootstrapApp, $name); })
+            ->whenNotEmpty(function ($names) use ($bootstrapApp) {
+                $names = $names->implode(PHP_EOL.'        ');
+
+                // Paso 2: Convertir todos los saltos de línea a "\n"
+//                $bootstrapApp = str_replace(["\r\n", "\n"], PHP_EOL, $bootstrapApp);
+
+                $bootstrapApp = str_replace(
+                    '->withExceptions(function (Exceptions $exceptions) {'.PHP_EOL."        //",
+                    '->withExceptions(function (Exceptions $exceptions) {'.PHP_EOL."        $names",
+                    $bootstrapApp
+                );
+
+                file_put_contents(base_path('bootstrap/app.php'), $bootstrapApp);
+            });
     }
 }
