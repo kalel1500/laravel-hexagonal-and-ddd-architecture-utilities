5db7d7e9c941cc1794297afecd6d2ddcf9eefcf7
Adrián <acanals@externos.itnow.es>
2024-11-11 08:58:51 +0100

    stubs: actualizado debido la nueva version de "laravel/laravel"

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -3,11 +3,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
         './node_modules/flowbite/**/*.js',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.js',
         './vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.blade.php',
     ],
