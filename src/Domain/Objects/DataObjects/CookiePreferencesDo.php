a78108ca40c30179b7ac3a3d52f3eba7b5220efc
Adrian Canals <a.canals14@gmail.com>
2024-11-20 21:33:14 +0100

    cookies: añadir la nueva config "hexagonal.sidebar_collapsed_default" en la clase "CookiePreferencesDo"

--- src/Domain/Objects/DataObjects/CookiePreferencesDo.php
+++ src/Domain/Objects/DataObjects/CookiePreferencesDo.php
@@ -7,28 +7,37 @@ namespace Thehouseofel\Hexagonal\Domain\Objects\DataObjects;
 final class CookiePreferencesDo extends ContractDataObject
 {
     protected $dark_mode_default;
+    protected $sidebar_collapsed_default;
     protected $sidebar_state_per_page;
 
     public function __construct(
         bool $dark_mode_default,
+        bool $sidebar_collapsed_default,
         bool $sidebar_state_per_page
     )
     {
-        $this->dark_mode_default      = $dark_mode_default;
-        $this->sidebar_state_per_page = $sidebar_state_per_page;
+        $this->dark_mode_default         = $dark_mode_default;
+        $this->sidebar_collapsed_default = $sidebar_collapsed_default;
+        $this->sidebar_state_per_page    = $sidebar_state_per_page;
     }
 
     protected static function createFromArray(array $data): self
     {
         return new self(
             $data['dark_mode_default'] ?? null,
-            $data['sidebar_state_per_page'] ?? null
+                $data['sidebar_collapsed_default'] ?? null,
+                $data['sidebar_state_per_page'] ?? null
         );
     }
 
     public function dark_mode_default(): bool
     {
         return $this->dark_mode_default;
+    }
+
+    public function sidebar_collapsed_default(): bool
+    {
+        return $this->sidebar_collapsed_default;
     }
 
     public function sidebar_state_per_page(): bool
@@ -39,6 +48,11 @@ final class CookiePreferencesDo extends ContractDataObject
     public function set_dark_mode_default(bool $value): void
     {
         $this->dark_mode_default = $value;
+    }
+
+    public function set_sidebar_collapsed_default(bool $value): void
+    {
+        $this->sidebar_collapsed_default = $value;
     }
 
     public function set_sidebar_state_per_page(bool $value): void
