b4c816c0f9b61daea82e3c090ce026fb5ffdf2be
Adrian Canals <a.canals14@gmail.com>
2024-11-22 01:42:05 +0100

    (fix) solucionar error vite poniendo el "publicDir" a "false" (ya que coincide con el "outDir")

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+    publicDir: false,
     build: {
         outDir: 'public/build',
         assetsDir: '',
