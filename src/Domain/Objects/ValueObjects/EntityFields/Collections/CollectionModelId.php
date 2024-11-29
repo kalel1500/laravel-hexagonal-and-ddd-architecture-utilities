1633b46ecaba02b70591ec326b828d0383351d22
Adrián <acanals@externos.itnow.es>
2024-11-29 14:46:14 +0100

    (breaking) Refactor: "CollectionModelId" movidas a "...Objects\ValueObjects\EntityFields\Collections"

--- src/Domain/Objects/ValueObjects/EntityFields/Collections/CollectionModelId.php
+++ src/Domain/Objects/ValueObjects/EntityFields/Collections/CollectionModelId.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Thehouseofel\Hexagonal\Domain\Objects\Collections;
+namespace Thehouseofel\Hexagonal\Domain\Objects\ValueObjects\EntityFields\Collections;
 
 use Thehouseofel\Hexagonal\Domain\Objects\Collections\Contracts\ContractCollectionVo;
 use Thehouseofel\Hexagonal\Domain\Objects\ValueObjects\EntityFields\Contracts\ContractModelId;
