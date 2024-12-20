fa312c0608b142799ea39b0d53b0edd164f6d84e
Adrian Canals <a.canals14@gmail.com>
2024-12-20 17:04:05 +0100

    Nueva Macro "mergeTailwind" en la clase "ComponentAttributeBag" para poder usar el "$attributes->mergeTailwind(...)" en los componentes

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 @props(['title', 'contentId' => ''])
 
-<div {{ $attributes->merge(['class' => 'block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 dark:shadow-2xl']) }}>
+<div {{ $attributes->mergeTailwind('block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 dark:shadow-2xl') }}>
     @isset($title)
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $title }}</h5>
     @endisset
