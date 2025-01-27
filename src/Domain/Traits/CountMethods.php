2cff606548288df766b1f264bd039c3d65bf001a
Adrian Canals <a.canals14@gmail.com>
2025-01-27 13:02:23 +0100

    Nuevo trait "CountMethods" para contar los métodos que tiene una clase

---
+++ src/Domain/Traits/CountMethods.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thehouseofel\Hexagonal\Domain\Traits;
+
+trait CountMethods
+{
+    protected function countPublicMethods(): int
+    {
+//        $numeroDeMetodos = count(get_class_methods($this));
+//        dd($numeroDeMetodos);
+
+        // Crear una instancia de ReflectionClass para la clase actual
+        $reflexion = new \ReflectionClass($this);
+
+        // Obtener los métodos públicos o protegidos (sin incluir los estáticos)
+        $methods = $reflexion->getMethods(\ReflectionMethod::IS_PUBLIC);
+
+        // Filtrar los métodos que NO sean estáticos
+        $filteredMethods = array_filter($methods, fn($method) => !$method->isStatic() && $method->getName() !== '__construct');
+
+        // Contar los métodos filtrados
+        return count($filteredMethods);
+    }
+}
