308c5c90a80b66a05fe479b4d9713818587a2713
Adrian Canals <a.canals14@gmail.com>
2025-02-06 12:46:04 +0100

    stubsExample: Añadir ruta "/dashboard" + Proteger rutas con el middleware "auth" + (refactor) Cambiar la ruta "/" de un "get" con callback al método "Route::redirect"

--- stubs/generate/routes/web.php
+++ stubs/generate/routes/web.php
@@ -1,15 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Src\Dashboard\Infrastructure\Http\Controllers\DashboardController;
 use Src\Shared\Infrastructure\Http\Controllers\DefaultController;
 
 /**
  * Ruta original de Laravel para la vista welcome
  */
+
 Route::get('/welcome', fn() => view('welcome'))->name('welcome');
 
 /**
  * Rutas de la aplicación
  */
-Route::get('/', fn() => redirect()->route('home'));
-Route::get('/home', [DefaultController::class, 'home'])->name('home');
+
+// Ruta base
+Route::redirect('/', '/home');
+
+// Rutas protegidas
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home',      [DefaultController::class, 'home'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+});
