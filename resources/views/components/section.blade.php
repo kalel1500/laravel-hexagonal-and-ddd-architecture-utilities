300dbe1866341928be8e9edefc88c66644eae01c
Adrian Canals <a.canals14@gmail.com>
2025-02-07 21:03:56 +0100

    layout: Intensificar sombra del componente "section"

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,3 +1,3 @@
-<section {{ $attributes->mergeTailwind('bg-white dark:bg-gray-800 p-5 rounded dark:text-gray-400 shadow') }}>
+<section {{ $attributes->mergeTailwind('bg-white dark:bg-gray-800 p-5 rounded dark:text-gray-400 shadow-[0_0_5px_2px_rgba(0,0,0,0.3)] dark:shadow-[0_0_5px_2px_rgba(0,0,0)]') }}>
     {{ $slot }}
 </section>
