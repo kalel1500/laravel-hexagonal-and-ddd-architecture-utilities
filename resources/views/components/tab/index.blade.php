3818a0ff22d1d251f12bd61ab0e16c29f685b47e
Adrian Canals <a.canals14@gmail.com>
2025-02-12 13:47:45 +0100

    layout: usar el "$attributes->mergeTailwind()" en el componente "hexagonal::tab" para poder añadir nuevas clases

--- resources/views/components/tab/index.blade.php
+++ resources/views/components/tab/index.blade.php
@@ -1,4 +1,4 @@
-<div class="border-b border-gray-200 text-center text-sm font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400">
+<div {{ $attributes->mergeTailwind('border-b border-gray-200 text-center text-sm font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400') }}>
     <ul class="-mb-px flex flex-wrap">
         {{ $slot }}
     </ul>
