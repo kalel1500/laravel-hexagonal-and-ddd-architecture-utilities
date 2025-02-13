666ad7072b48c3ca0183dee0b55548cadf0516ea
Adrian Canals <a.canals14@gmail.com>
2025-02-14 00:24:01 +0100

    layout: Nuevo parámetro "size" en el componente "hexagonal::button"

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,8 +1,15 @@
-@props(['color' => 'blue'])
+@props(['color' => 'blue', 'size' => 'base'])
 
 @php
-    $classes = 'mb-2 rounded-lg px-5 py-2.5 text-sm font-medium focus:outline-none focus:ring-4';
-    $arrayBtnClasses = [
+    $classes = 'mb-2 rounded-lg font-medium focus:outline-none focus:ring-4';
+    $sizeClasses = [
+        'xs'    => 'px-3 py-2   text-xs  ',
+        'sm'    => 'px-3 py-2   text-sm  ',
+        'base'  => 'px-5 py-2.5 text-sm  ',
+        'lg'    => 'px-5 py-3   text-base',
+        'lx'    => 'px-6 py-3.5 text-base',
+    ];
+    $colorClasses = [
         'blue'      => '                       bg-blue-700     text-white      hover:bg-blue-800   focus:ring-blue-300                             dark:bg-blue-600                                                dark:hover:bg-blue-700      dark:focus:ring-blue-800',
         'dark'      => '                       bg-gray-800     text-white      hover:bg-gray-900   focus:ring-gray-300     dark:border-gray-700    dark:bg-gray-800                                                dark:hover:bg-gray-700      dark:focus:ring-gray-700',
         'green'     => '                       bg-green-700    text-white      hover:bg-green-800  focus:ring-green-300                            dark:bg-green-600                                               dark:hover:bg-green-700     dark:focus:ring-green-800',
@@ -12,7 +19,7 @@
         'yellow'    => '                       bg-yellow-400   text-white      hover:bg-yellow-500 focus:ring-yellow-300                                                                                                                       dark:focus:ring-yellow-900',
         'gray'      => '                       bg-gray-500     text-white      hover:bg-gray-600   focus:ring-gray-300                                                                                                                         dark:focus:ring-gray-900',
     ];
-    $btnClasses = $arrayBtnClasses[$color];
+    $finalClasses = $classes.' '.$sizeClasses[$size].' '.$colorClasses[$color];
 @endphp
 
-<button type="button" {{ $attributes->mergeTailwind($classes.' '.$btnClasses) }}>{{ $slot }}</button>
+<button type="button" {{ $attributes->mergeTailwind($finalClasses) }}>{{ $slot }}</button>
