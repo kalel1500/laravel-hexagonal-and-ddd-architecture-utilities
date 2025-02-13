190de00a941d51bba5a7663c0bb0e2f1a7f6c742
Adrian Canals <a.canals14@gmail.com>
2025-02-13 09:17:09 +0100

    (revert) stubsExample: Eliminar los archivos del front que aún no han cambiado para facilitar el mantenimiento

--- stubs/generate/front/resources/js/src/shared/application/HomeUseCase.ts
+++
@@ -1,8 +0,0 @@
-import { Instantiable } from "@kalel1500/laravel-ts-utils";
-
-export default class HomeUseCase extends Instantiable
-{
-    __invoke() {
-        console.log('inicio');
-    }
-}
--- stubs/generate/front/resources/js/src/shared/infrastructure/DefaultController.ts
+++
@@ -1,7 +0,0 @@
-import HomeUseCase from '../application/HomeUseCase';
-
-export default class DefaultController {
-    home() {
-        HomeUseCase.new().__invoke();
-    }
-}
