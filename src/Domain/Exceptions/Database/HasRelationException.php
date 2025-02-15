35db27183d8fc19aa5841a4ef1763991fbc4cbbd
Adrian Canals <a.canals14@gmail.com>
2025-02-15 01:26:04 +0100

    Añadir constructor a la excepción "HasRelationException" para recibir parámetros y setear un mensaje

--- src/Domain/Exceptions/Database/HasRelationException.php
+++ src/Domain/Exceptions/Database/HasRelationException.php
@@ -9,4 +9,9 @@ use Thehouseofel\Hexagonal\Domain\Exceptions\Base\BasicException;
 final class HasRelationException extends BasicException
 {
     const STATUS_CODE = 409; // HTTP_CONFLICT;
+
+    public function __construct(string $model, string $relation)
+    {
+        parent::__construct(__('database_recordIsUsedInRelation_:model_:relation', ['model' => $model, 'relation' => $relation]));
+    }
 }
