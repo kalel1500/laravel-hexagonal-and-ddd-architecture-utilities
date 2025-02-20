d276f0d17911e7e4c54738cdd204c84ac17b4e27
Adrian Canals <a.canals14@gmail.com>
2025-02-20 15:11:05 +0100

    (refactor) Exceptions: Definir la constante "STATUS_CODE = 403" en la clase "UnauthorizedException" para no ponerlo a mano en cada método

--- src/Domain/Exceptions/UnauthorizedException.php
+++ src/Domain/Exceptions/UnauthorizedException.php
@@ -9,6 +9,7 @@ use Thehouseofel\Hexagonal\Domain\Objects\Entities\UserEntity;
 
 class UnauthorizedException extends BasicHttpException
 {
+    const STATUS_CODE = 403;
 
     public static function forRoles(string $roles): self
     {
@@ -18,7 +19,7 @@ class UnauthorizedException extends BasicHttpException
             $message .= ' '.__('h::auth.necessary_roles', ['roles' => $roles]);
         }
 
-        return new static(403, $message);
+        return new static(static::STATUS_CODE, $message);
     }
 
     public static function forPermissions(string $permissions): self
@@ -29,7 +30,7 @@ class UnauthorizedException extends BasicHttpException
             $message .= ' '.__('h::auth.necessary_permissions', ['permissions' => $permissions]);
         }
 
-        return new static(403, $message);
+        return new static(static::STATUS_CODE, $message);
     }
 
     public static function missingTraitHasPermissions(UserEntity $user): self
@@ -41,7 +42,7 @@ class UnauthorizedException extends BasicHttpException
 
     public static function notLoggedIn(): self
     {
-        return new static(403, __('h::auth.not_logged_in'));
+        return new static(static::STATUS_CODE, __('h::auth.not_logged_in'));
     }
 
 }
