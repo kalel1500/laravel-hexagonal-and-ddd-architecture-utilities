d44e668f5cedee5c3a789047d230fe581b68fbe1
Adrian Canals <a.canals14@gmail.com>
2025-03-05 22:51:55 +0100

    (breaking) renamePackage: rutas renombradas

--- resources/views/components/navbar/brand.blade.php
+++ resources/views/components/navbar/brand.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('hexagonal.root') }}" class="mr-4 flex items-center justify-between lg:mr-20">
+<a href="{{ route('kalion.root') }}" class="mr-4 flex items-center justify-between lg:mr-20">
     <img src="@viteAsset('resources/images/logo.svg')" width="32" height="33" class="mr-3 h-8" alt="{{ config('app.name') }} Logo"/>
     <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">{{ config('app.name') }}</span>
 </a>
