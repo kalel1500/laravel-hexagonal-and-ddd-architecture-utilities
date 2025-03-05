810d2f7d490b9322f02d36223971c50361911487
Adrian Canals <a.canals14@gmail.com>
2025-03-05 04:31:27 +0100

    darkTheme: "<script>" en el HTML para cargar el DarkMode rápidamente (sirve para evitar ver saltos de color cuando la conexión es lenta y el "theme" es "system", ya que el código compilado tarda más en cargar)

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -39,6 +39,15 @@
             @endif
         @endif
 
+        <!-- JavaScript para cargar el DarkMode -->
+        <script>
+            const theme = document.querySelector('html').getAttribute('color-theme');
+            if (theme === 'dark' || (theme === 'system' && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+                document.documentElement.classList.add("dark");
+                document.documentElement.setAttribute("data-theme", "dark");
+            }
+        </script>
+
         <!-- Estilos de cada vista -->
         @stack('styles')
 
